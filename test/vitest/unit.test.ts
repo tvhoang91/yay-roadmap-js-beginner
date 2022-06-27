3edd652c941f7cb86a0f6cc6ec0f64be701035a3
Hoang Truong <tvhoang91@gmail.com>
2022-06-27 12:02:41 +0700

    eslint

--- test/vitest/unit.test.ts
+++ test/vitest/unit.test.ts
@@ -1,37 +1,37 @@
-import { assert, describe, expect, it } from 'vitest'
+import { assert, describe, expect, it } from "vitest"
 
-describe('Math', () => {
-  it('+ operator', () => {
+describe("Math", () => {
+  it("+ operator", () => {
     expect(1 + 1).eq(2)
   })
 
-  it('Math.sqrt()', () => {
+  it("Math.sqrt()", () => {
     expect(Math.sqrt(4)).eq(2)
   })
 
-  it('Math.sqrt() 3 times', () => {
+  it("Math.sqrt() 3 times", () => {
     expect(Math.sqrt(4)).toBe(2)
     expect(Math.sqrt(144)).toBe(12)
     expect(Math.sqrt(2)).toBe(Math.SQRT2)
   })
 })
 
-describe('Snapshot', () => {
-  it('Snapshot Object', () => {
-    expect({ foo: 'bar' }).toMatchSnapshot()
+describe("Snapshot", () => {
+  it("Snapshot Object", () => {
+    expect({ foo: "bar" }).toMatchSnapshot()
   })
 })
 
-describe('JSON', () => {
-  it('JSON.stringify()', () => {
+describe("JSON", () => {
+  it("JSON.stringify()", () => {
     const input = {
-      foo: 'hello',
-      bar: 'world',
+      foo: "hello",
+      bar: "world",
     }
   
     const output = JSON.stringify(input)
   
     expect(output).eq('{"foo":"hello","bar":"world"}')
-    assert.deepEqual(JSON.parse(output), input, 'matches original')
+    assert.deepEqual(JSON.parse(output), input, "matches original")
   })
 })
